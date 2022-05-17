ee9578a15586af72b4910076b0846e2209482718
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-17 18:00:13 -0300

    fix

--- pi-server/status.py
+++ pi-server/status.py
@@ -4,10 +4,10 @@ import time
 def status_aberturas(gp):
 
     INPUT_ABERTURA_1 = 18
-    gp.setup(INPUT_ABERTURA_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    gp.setup(INPUT_ABERTURA_1, gp.IN, pull_up_down=gp.PUD_UP)
 
     OUTPUT_ABERTURA = 21 
-    gp.setup(OUTPUT_ABERTURA, GPIO.OUT)
+    gp.setup(OUTPUT_ABERTURA, gp.OUT)
 
     abertura_abierta = bool(gp.input(INPUT_ABERTURA_1))
 
