aa371963f923fdb1b66af6aedba7bcb0e6f7d31e
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-18 10:49:06 -0300

    restaet

--- restart_pi-server.py
+++ restart_pi-server.py
@@ -13,13 +13,16 @@ rpi_url = 'https://light-panda-16.telebit.io/status'
 cantidad_maxima_intentos = 100
 
 while True:
+    print("Verificamos si el server está arriba")
     r = requests.get(rpi_url)
     cod = r.status_code
     res = r.json()
 
     if cod == 200 and res["status"] == 'Ok' and cantidad_maxima_intentos > 0:
+        print("Está funcionando")
         break
     
+    print("Intentamos levantarlo")
     restartTunnel = "telebit restart"
     process = subprocess.Popen(restartTunnel.split(), stdout=subprocess.PIPE,cwd=path_telebid)
     cantidad_maxima_intentos = cantidad_maxima_intentos - 1
