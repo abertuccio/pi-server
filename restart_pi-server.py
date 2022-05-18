5b1b4848440de0954ec499701e47ea1a5d64a026
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-18 11:50:35 -0300

    fix

--- restart_pi-server.py
+++ restart_pi-server.py
@@ -4,17 +4,17 @@ import subprocess
 
 path_telebid = '/home/pi/'
 
-# print("Restarteamos Docker")
+print("Restarteamos Docker")
 
-# restartDocker = "docker restart pi-server"
-# process = subprocess.Popen(restartDocker.split(), stdout=subprocess.PIPE)
-# outs, errs = process.communicate()
+restartDocker = "docker restart pi-server"
+process = subprocess.Popen(restartDocker.split(), stdout=subprocess.PIPE)
+outs, errs = process.communicate()
 
-# print("Docker iniciado: ", str(outs))
-# print("Errores: ",errs)
+print("Docker iniciado: ", str(outs))
+print("Errores: ",errs)
 
-# print("Esperamos 20 segudos para intentar telebit")
-# time.sleep(20)
+print("Esperamos 20 segudos para intentar telebit")
+time.sleep(20)
 
 rpi_url = 'https://light-panda-16.telebit.io/status'
 cantidad_maxima_intentos = 100
@@ -41,7 +41,7 @@ while True:
         break
 
     print("Intentamos levantarlo")
-    restartTunnel = "sudo -u www-data bash -c '/home/pi/telebit restart'"
+    restartTunnel = "sudo -u www-data bash -c \'/home/pi/telebit restart\'"
     process = subprocess.Popen(restartTunnel.split(), stdout=subprocess.PIPE,cwd=path_telebid)
     cantidad_maxima_intentos = cantidad_maxima_intentos - 1
     time.sleep(10)
