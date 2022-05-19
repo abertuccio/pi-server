6edaefb891155a59c009604052c672e165e72c2f
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-19 15:52:35 -0300

    print en archivo

--- restart_pi-server.py
+++ restart_pi-server.py
@@ -4,18 +4,24 @@ import time
 import subprocess
 import datetime
 
+f = open('home/pi/pi-server/cron.log', 'w')
+
 path_telebid = '/home/pi/'
 
 print(datetime.datetime.now()," Restarteamos Docker")
+f.write(datetime.datetime.now()," Restarteamos Docker")
 
 restartDocker = "docker restart pi-server"
 process = subprocess.Popen(restartDocker.split(), stdout=subprocess.PIPE)
 outs, errs = process.communicate()
 
 print(datetime.datetime.now()," Docker iniciado: ", str(outs))
+f.write(datetime.datetime.now()," Docker iniciado: ", str(outs))
 print(datetime.datetime.now()," Errores: ",errs)
+f.write(datetime.datetime.now()," Errores: ",errs)
 
 print(datetime.datetime.now()," Esperamos 20 segudos para intentar telebit")
+f.write(datetime.datetime.now()," Esperamos 20 segudos para intentar telebit")
 time.sleep(20)
 
 rpi_url = 'https://stale-octopus-48.telebit.io/status'
@@ -38,6 +44,7 @@ requests.get(telegram_URL+'&text=Se reinicia RPI, IP: '+ip())
 
 while True:
     print(datetime.datetime.now()," Verificamos si el server está arriba")
+    f.write(datetime.datetime.now()," Verificamos si el server está arriba")
     r = requests.get(rpi_url, verify=False, timeout=5)
     cod = r.status_code
     res = {}
@@ -53,6 +60,7 @@ while True:
     if cod == 200 and res["status"] == 'Ok':
         requests.get(telegram_URL+'&text=El server de la alarma está operativo.')
         print(datetime.datetime.now()," Está funcionando")
+        f.write(datetime.datetime.now()," Está funcionando")
         break
 
     if not cantidad_maxima_intentos:
@@ -60,10 +68,13 @@ while True:
         break
 
     print(datetime.datetime.now()," Parece no responder, intentamos levantarlo, Intentos: ",cantidad_maxima_intentos)
+    f.write(datetime.datetime.now()," Parece no responder, intentamos levantarlo, Intentos: ",cantidad_maxima_intentos)
     restartTunnel = '/home/pi/telebit restart'
     process = subprocess.Popen(restartTunnel.split(), stdout=subprocess.PIPE)
 
 
     cantidad_maxima_intentos = cantidad_maxima_intentos - 1
     time.sleep(20)
+
+f.close()
 
