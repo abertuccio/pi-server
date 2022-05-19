98059cf4f74165fc05fb4095a0eea1a8f6f8daf7
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-19 16:26:15 -0300

    deb

--- restart_pi-server.py
+++ restart_pi-server.py
@@ -4,24 +4,24 @@ import time
 import subprocess
 import datetime
 
-f = open('/home/pi/pi-server/cron_py.log', 'w')
+# f = open('/home/pi/pi-server/cron_py.log', 'w')
 
 path_telebid = '/home/pi/'
 
 print(datetime.datetime.now()," Restarteamos Docker")
-f.write(datetime.datetime.now() + " Restarteamos Docker")
+# f.write(datetime.datetime.now() + " Restarteamos Docker")
 
 restartDocker = "docker restart pi-server"
 process = subprocess.Popen(restartDocker.split(), stdout=subprocess.PIPE)
 outs, errs = process.communicate()
 
 print(datetime.datetime.now()," Docker iniciado: ", str(outs))
-f.write(datetime.datetime.now() + " Docker iniciado: ", str(outs))
+# f.write(datetime.datetime.now() + " Docker iniciado: ", str(outs))
 print(datetime.datetime.now()," Errores: ",errs)
-f.write(datetime.datetime.now() + " Errores: ",errs)
+# f.write(datetime.datetime.now() + " Errores: ",errs)
 
 print(datetime.datetime.now()," Esperamos 20 segudos para intentar telebit")
-f.write(datetime.datetime.now() + " Esperamos 20 segudos para intentar telebit")
+# f.write(datetime.datetime.now() + " Esperamos 20 segudos para intentar telebit")
 time.sleep(20)
 
 rpi_url = 'https://stale-octopus-48.telebit.io/status'
@@ -44,7 +44,7 @@ requests.get(telegram_URL+'&text=Se reinicia RPI, IP: '+ip())
 
 while True:
     print(datetime.datetime.now()," Verificamos si el server está arriba")
-    f.write(datetime.datetime.now() + " Verificamos si el server está arriba")
+    # f.write(datetime.datetime.now() + " Verificamos si el server está arriba")
     r = requests.get(rpi_url, verify=False, timeout=5)
     cod = r.status_code
     res = {}
@@ -60,16 +60,16 @@ while True:
     if cod == 200 and res["status"] == 'Ok':
         requests.get(telegram_URL+'&text=El server de la alarma está operativo.')
         print(datetime.datetime.now()," Está funcionando")
-        f.write(datetime.datetime.now() + " Está funcionando")
+        # f.write(datetime.datetime.now() + " Está funcionando")
         break
 
     if not cantidad_maxima_intentos:
         print(datetime.datetime.now()," Alcanzamos la máxima cantidad de intentos.")
-        f.write(datetime.datetime.now() + " Alcanzamos la máxima cantidad de intentos.")
+        # f.write(datetime.datetime.now() + " Está funcionando")
         break
 
     print(datetime.datetime.now()," Parece no responder, intentamos levantarlo, Intentos: ",cantidad_maxima_intentos)
-    f.write(datetime.datetime.now()," Parece no responder, intentamos levantarlo, Intentos: ",cantidad_maxima_intentos)
+    # f.write(datetime.datetime.now()," Parece no responder, intentamos levantarlo, Intentos: ",cantidad_maxima_intentos)
     restartTunnel = '/home/pi/telebit restart'
     process = subprocess.Popen(restartTunnel.split(), stdout=subprocess.PIPE)
 
@@ -77,5 +77,5 @@ while True:
     cantidad_maxima_intentos = cantidad_maxima_intentos - 1
     time.sleep(20)
 
-f.close()
+# f.close()
 
