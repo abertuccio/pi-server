238e28279f70b615cfaef078f57cc05640885be2
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-21 17:47:47 -0300

    por ahora sacamos el hash server

--- pi-server/auth.py
+++ pi-server/auth.py
@@ -36,6 +36,7 @@ def getTotp():
     totp = pyotp.TOTP(os.environ['HASH_EMPAREJAMIENTO'])
     return("Current OTP:" + totp.now() + " " + hora)
 
+
 def generarToken():
 
     minutos_validez_token_defecto = 20
@@ -44,20 +45,23 @@ def generarToken():
 
     random = str(os.urandom(44).hex())
 
-    token = hashlib.sha512( str( mt +  random).encode("utf-8") ).hexdigest()
+    token = hashlib.sha512(str(mt + random).encode("utf-8")).hexdigest()
 
-    segundos_validez_token = int(time.time()) + (minutos_validez_token_defecto * 60)
+    segundos_validez_token = int(time.time()) + \
+        (minutos_validez_token_defecto * 60)
 
-    return {"token":token,"tiempo":segundos_validez_token}
+    return {"token": token, "tiempo": segundos_validez_token}
+
 
 def login(parametros):
 
-    hash_server = "0012ac79f4559fd080423be618d8ff44c7920f30e73ecf37654041183996fb20"
+    # hash_server = "0012ac79f4559fd080423be618d8ff44c7920f30e73ecf37654041183996fb20"
 
-    if ("hash_server" not in parametros or
-        "usuario" not in parametros or
+    # if ("hash_server" not in parametros or
+    if ("usuario" not in parametros or
             "password" not in parametros):
-        raise Error('No se enviaron los parámetros mínimos de autenticación.')
+        raise Error(
+            'No se enviaron los parámetros mínimos de autenticación.')
 
     if parametros["hash_server"] != hash_server:
         raise Error('No es una ubicación permitida para hacer login.')
@@ -71,10 +75,10 @@ def login(parametros):
         raise Error('La combinacion de usuario y password no es correcta.')
 
     if usuarioDBD[0]["password"] != parametros["password"]:
-        raise Error('La combinacion de usuario y password no es correcta..') 
+        raise Error('La combinacion de usuario y password no es correcta..')
 
     token = generarToken()
 
-    db.update({'token': token}, Usuario.usuario == parametros["usuario"])  
+    db.update({'token': token}, Usuario.usuario == parametros["usuario"])
 
-    return {"respuesta":token,"status":"Ok"}
+    return {"respuesta": token, "status": "Ok"}
