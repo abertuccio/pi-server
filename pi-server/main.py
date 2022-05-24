ae8e58c5a5c47ec4e4b552456962adc52842ca5f
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-24 11:08:29 -0300

    deb

--- pi-server/main.py
+++ pi-server/main.py
@@ -28,8 +28,8 @@ def status_fn():
 
     stat = {}
     stat["server"] = "El server RPI funciona correctamente"
-    stat["abertura_abierta"] = status_aberturas()
-    stat["status_alarma"] = "algo" #getStatusAlarma()
+    stat["abertura_abierta"] = "algo" # status_aberturas()
+    stat["status_alarma"] = getStatusAlarma()
 
     return {"status":"Ok","respuesta":stat}
 
