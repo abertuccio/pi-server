6103e38ea93b1623c914869a83288132cff40fca
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-25 11:24:29 -0300

    dev

--- pi-server/buzeer.py
+++ pi-server/buzeer.py
@@ -7,10 +7,10 @@ BUZZER = 4
 b = TonalBuzzer(BUZZER)
 
 b.play(Tone("A3"))
-time.sleep(1)
+time.sleep(0.7)
 b.stop()
 time.sleep(1)
-b.play(Tone("B4"))
+b.play(Tone("A#3"))
 time.sleep(0.7)
 b.play(Tone("E4"))
 time.sleep(0.4)
