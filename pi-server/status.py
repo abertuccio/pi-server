5536d0c3c8ac3694a5cf09a479a2874c9adedab2
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-05-26 12:09:05 -0300

    deb

--- pi-server/status.py
+++ pi-server/status.py
@@ -29,7 +29,7 @@ def getStatusAlarma():
         newdata = filedata.replace('^@', '')
 
         f = open('/app/db/status.json','w')
-        f.write("pepe")
+        f.write('{"_default": {"1": {"debe_sonar_alarma": true, "status_alarma": "ARMADO"}}}')
         f.close()
             
         db = TinyDB('/app/db/status.json')
