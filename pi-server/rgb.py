73079f058d2e402e5c75a4a08828a571739e9b52
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-06-07 10:32:25 -0300

    deb

--- pi-server/rgb.py
+++ pi-server/rgb.py
@@ -1,117 +1,15 @@
-#Program asks for user input to determine color to shine.
-
-import time, sys
 import RPi.GPIO as GPIO
+import time
 
-redPin = 33   #Set to appropriate GPIO
-greenPin = 31 #Should be set in the 
-bluePin = 26  #GPIO.BOARD format
+GPIO.setmode(GPIO.BCM)
 
-def blink(pin):
-    GPIO.setmode(GPIO.BOARD)
-    
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.HIGH)
-    
-def turnOff(pin):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
-    
-def redOn():
-    blink(redPin)
+LED_ROJO = 13
+LED_VERDE = 6
+LED_VERDE = 26
 
-def redOff():
-    turnOff(redPin)
+GPIO.setup(LED_ROJO, GPIO.OUT)
+GPIO.output(LED_ROJO, GPIO.HIGH)
+time.sleep(2)
+GPIO.output(LED_ROJO, GPIO.LOW)
 
-def greenOn():
-    blink(greenPin)
-
-def greenOff():
-    turnOff(greenPin)
-
-def blueOn():
-    blink(bluePin)
-
-def blueOff():
-    turnOff(bluePin)
-
-def yellowOn():
-    blink(redPin)
-    blink(greenPin)
-
-def yellowOff():
-    turnOff(redPin)
-    turnOff(greenPin)
-
-def cyanOn():
-    blink(greenPin)
-    blink(bluePin)
-
-def cyanOff():
-    turnOff(greenPin)
-    turnOff(bluePin)
-
-def magentaOn():
-    blink(redPin)
-    blink(bluePin)
-
-def magentaOff():
-    turnOff(redPin)
-    turnOff(bluePin)
-
-def whiteOn():
-    blink(redPin)
-    blink(greenPin)
-    blink(bluePin)
-
-def whiteOff():
-    turnOff(redPin)
-    turnOff(greenPin)
-    turnOff(bluePin)
-    
-print("""Ensure the following GPIO connections: R-11, G-13, B-15
-Colors: Red, Green, Blue, Yellow, Cyan, Magenta, and White
-Use the format: color on/color off""")
-
-def main():
-    while True:
-        cmd = raw_input("-->")
-
-
-        if cmd == "red on":
-            redOn()
-        elif cmd == "red off":
-            redOff()
-        elif cmd == "green on":
-            greenOn()
-        elif cmd == "green off":
-            greenOff()
-        elif cmd == "blue on":
-            blueOn()
-        elif cmd == "blue off":
-            blueOff()
-        elif cmd == "yellow on":
-            yellowOn()
-        elif cmd == "yellow off":
-            yellowOff()
-        elif cmd == "cyan on":
-            cyanOn()
-        elif cmd == "cyan off":
-            cyanOff()
-        elif cmd == "magenta on":
-            magentaOn()
-        elif cmd == "magenta off":
-            magentaOff()
-        elif cmd == "white on":
-            whiteOn()
-        elif cmd == "white off":
-            whiteOff()
-        else:
-            print("Not a valid command")
-        
-        
-    return
-    
-
-main()
+GPIO.cleanup()
