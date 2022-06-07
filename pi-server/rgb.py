2d890e38e0176203dfd85204b47fa6d66af8a8bf
Andres Bertuccio <abertuccio@sintys.gob.ar>
2022-06-07 11:03:12 -0300

    deb

--- pi-server/rgb.py
+++ pi-server/rgb.py
@@ -1,15 +1,73 @@
+#libraries
 import RPi.GPIO as GPIO
-import time
-
+from time import sleep
+#disable warnings (optional)
+GPIO.setwarnings(False)
+#Select GPIO Mode
 GPIO.setmode(GPIO.BCM)
+#set red,green and blue pins
+redPin = 12
+greenPin = 19
+bluePin = 13
+#set pins as outputs
+GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(greenPin,GPIO.OUT)
+GPIO.setup(bluePin,GPIO.OUT)
 
-LED_ROJO = 13
-LED_VERDE = 6
-LED_VERDE = 26
+def turnOff():
+    GPIO.output(redPin,GPIO.HIGH)
+    GPIO.output(greenPin,GPIO.HIGH)
+    GPIO.output(bluePin,GPIO.HIGH)
+    
+def white():
+    GPIO.output(redPin,GPIO.LOW)
+    GPIO.output(greenPin,GPIO.LOW)
+    GPIO.output(bluePin,GPIO.LOW)
+    
+def red():
+    GPIO.output(redPin,GPIO.LOW)
+    GPIO.output(greenPin,GPIO.HIGH)
+    GPIO.output(bluePin,GPIO.HIGH)
 
-GPIO.setup(LED_ROJO, GPIO.OUT)
-GPIO.output(LED_ROJO, GPIO.LOW)
-time.sleep(2)
-GPIO.output(LED_ROJO, GPIO.HIGH)
-
-GPIO.cleanup()
+def green():
+    GPIO.output(redPin,GPIO.HIGH)
+    GPIO.output(greenPin,GPIO.LOW)
+    GPIO.output(bluePin,GPIO.HIGH)
+    
+def blue():
+    GPIO.output(redPin,GPIO.HIGH)
+    GPIO.output(greenPin,GPIO.HIGH)
+    GPIO.output(bluePin,GPIO.LOW)
+    
+def yellow():
+    GPIO.output(redPin,GPIO.LOW)
+    GPIO.output(greenPin,GPIO.LOW)
+    GPIO.output(bluePin,GPIO.HIGH)
+    
+def purple():
+    GPIO.output(redPin,GPIO.LOW)
+    GPIO.output(greenPin,GPIO.HIGH)
+    GPIO.output(bluePin,GPIO.LOW)
+    
+def lightBlue():
+    GPIO.output(redPin,GPIO.HIGH)
+    GPIO.output(greenPin,GPIO.LOW)
+    GPIO.output(bluePin,GPIO.LOW)
+    
+while True:
+    turnOff()
+    sleep(1) #1second
+    white()
+    sleep(1)
+    red()
+    sleep(1)
+    green()
+    sleep(1)
+    blue()
+    sleep(1)
+    yellow()
+    sleep(1)
+    purple()
+    sleep(1)
+    lightBlue()
+    sleep(1)
